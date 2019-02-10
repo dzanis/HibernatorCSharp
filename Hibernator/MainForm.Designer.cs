c6086b4f593375f07ebbcf35416551151e831d8d
dzanis <zanisdukalskis@gmail.com>
2019-02-10 13:42:15 +0200

    Добавьте файлы проекта.

---
+++ Hibernator/MainForm.Designer.cs
@@ -0,0 +1,114 @@
+﻿namespace Hibernator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.Text = "Hibernator";
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(204, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(79, 17);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.Text = "TimerInvert";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(83, 12);
+            this.trackBar1.Maximum = 99;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "minutesOff 0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 60);
+            this.label2.MaximumSize = new System.Drawing.Size(300, 130);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(324, 121);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.checkBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Hibernator/MainForm.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading; //Именно это пространство имен поддерживает многопоточность
+using Microsoft.Win32;
+
+namespace Hibernator
+{
+
+    public partial class MainForm : Form
+    {
+
+        public int minutesOff = 30;//через сколько минут выключить, от 1 до 99 минут
+        public bool timerinvert = false;//сколько минут нет активности или сколько осталось до гибернации
+        public bool PowerModesResume = false;// будет true при выходе из гибернации
+
+        string textInfo = "При бездействии пользователя,\n гибернизация начнётся через {0} мин \nСвернуть для фоновой работы. \nВ трее индикация бездействия в минутах\n";
+        Hibernator hibernator;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            Text = "Hibernator 2019.02.10";
+            this.StartPosition = FormStartPosition.CenterScreen;
+            SystemEvents.PowerModeChanged += OnPowerChange;
+            Application.ApplicationExit += new EventHandler(ApplicationExit);
+
+            if (Properties.Settings.Default.firstStart)
+            {   // если самый первый старт приложения
+                Properties.Settings.Default.firstStart = false;
+                Properties.Settings.Default.Save();
+                Properties.Settings.Default.Upgrade();
+            }
+            else
+            {   // если не первый старт приложения то запуск в свёрнутом виде
+                this.WindowState = FormWindowState.Minimized;
+                this.ShowInTaskbar = false;
+            }
+
+            // load settings
+            timerinvert = Properties.Settings.Default.timerinvert;
+            minutesOff = Properties.Settings.Default.minutesOff;
+
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {        
+            checkBox1.Checked = timerinvert;
+            trackBar1.Value = minutesOff;
+            label1.Text = String.Format("minutesOff {0}", minutesOff);
+            label2.Text = String.Format(textInfo, minutesOff);           
+            hibernator = new Hibernator(this);
+            hibernator.Start();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+            {
+                if (MessageBox.Show("Do you really want to exit?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Information) != DialogResult.Yes)
+                {
+                e.Cancel = true;
+                }       
+            }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+            {           
+                if (this.WindowState == FormWindowState.Minimized)
+                {
+                    Hide();
+                    setIconNumber(0);
+                    notifyIcon.Visible = true;
+                }
+            }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            timerinvert = checkBox1.Checked;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            minutesOff = trackBar1.Value;
+            label1.Text = String.Format("minutesOff {0}", minutesOff);
+            label2.Text = String.Format(textInfo, minutesOff);
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            notifyIcon.Visible = false;
+        }
+
+
+        private void setIconNumber(int number)
+        {
+            number = !timerinvert ? number : minutesOff - number;
+            // Create a bitmap and draw text on it
+            Bitmap bitmap = new Bitmap(32, 32);
+            Graphics graphics = Graphics.FromImage(bitmap);
+            string str = number > 9 ? "" + number : " " + number;
+            graphics.DrawString(str, new Font("System", 20, FontStyle.Regular), Brushes.White, 0, 0);
+            // Convert the bitmap with text to an Icon
+            Icon icon = Icon.FromHandle(bitmap.GetHicon());
+            notifyIcon.Icon = icon;
+        }
+
+        static int prevlastInputTime = 0;
+        //обновление иконки в трее
+        public void notyfyiconUpdate(int lastInputTime)
+        {
+            // lastInputTime - последняя активность пользователя в минутах
+            // такая логика обновляет иконку не чаще одного раза в минуту
+            if ((lastInputTime == 0 && prevlastInputTime > 0) ||
+                (lastInputTime - prevlastInputTime >= 1))
+            {
+                setIconNumber(lastInputTime);
+                Console.WriteLine("lastInputTime == " + lastInputTime);
+            }
+            prevlastInputTime = lastInputTime;
+        }
+
+
+        void ApplicationExit(object sender, EventArgs e)
+        {
+            // Сохраняем переменные. 
+            Properties.Settings.Default.timerinvert = timerinvert;
+            Properties.Settings.Default.minutesOff = minutesOff;
+            Properties.Settings.Default.Save();
+            // останавливаю поток гибернатора
+            hibernator.Stop();
+
+        }
+
+
+        // https://stackoverflow.com/questions/18206183/event-to-detect-system-wake-up-from-sleep-in-c-sharp
+        private void OnPowerChange(object s, PowerModeChangedEventArgs e)
+        {
+            switch (e.Mode)
+            {
+                case PowerModes.Resume:
+                    //MessageBox.Show("PowerModes.Resume");
+                    PowerModesResume = true;
+                    hibernator.Start();
+                    break;
+                case PowerModes.Suspend:
+                    //MessageBox.Show("PowerModes.Suspend");
+                    hibernator.Stop();// не уверен нужно ли останавливать поток перед гибернацией
+                    break;
+            }
+        }
+
+    }
+
+
+    public class Hibernator
+    {
+
+        private Thread myThread;
+        private MainForm main;
+
+        public Hibernator(MainForm main)
+        {
+            this.main = main;
+        }
+
+        public void Start()
+        {
+            myThread = new Thread(thread_func); //Создаем новый объект потока (Thread)
+            myThread.IsBackground = true;// что-бы поток закрывался вместе с приложением
+            myThread.Start(); //запускаем поток
+        }
+
+        /// остановить поток, например при закрытии приложения
+        public void Stop()
+        {
+            myThread.Abort();
+            myThread.Join();
+            myThread = null;
+        }
+
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+
+        [System.Runtime.InteropServices.DllImport("user32.dll")]
+        static extern bool SetWindowText(IntPtr hWnd, string lpString);
+
+        [DllImport("user32.dll")]
+        public static extern int SendMessageW(IntPtr hWnd, int wMsg, IntPtr wParam, IntPtr lParam);
+
+        [DllImport("user32.dll")]
+        static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+
+        /// ожидание отмены пред гибернизацией с обратным отсчётом на кнопке
+        public void message_thread_func()
+        {
+            const int WM_COMMAND = 0x0111;
+            const int IDYES = 6;
+            const int IDNO = 7;
+            const int BN_CLICKED = 0;
+
+            Thread.Sleep(1000);// ожидание до вызова окна сообщения
+
+            IntPtr hwndMsgBox = FindWindow(null, "HibernateConfirm");
+            IntPtr hwndButton = FindWindowEx(hwndMsgBox, (IntPtr)0, "Button", null);
+            const int SW_HIDE = 0;
+            ShowWindow(hwndButton, SW_HIDE);
+
+            if (hwndMsgBox != null)
+            {
+                // длбавляем обратный отсчёт на титле
+                for (int i = 30; i > 0; i--) // отсчёт 30 секунд
+                {
+                    if ( ((GetLastInputTime() / 60) < main.minutesOff) &&  // если была активность мышки или клавы
+                        !main.PowerModesResume) // при выходе из гибернации отмена только нажатием на кнопку 
+                        SendMessageW(hwndMsgBox, WM_COMMAND, (IntPtr)(IDNO | (BN_CLICKED << 16)), hwndButton); //то симулируем нажатие "Нет"
+                    string title_text = "HibernateConfirm " + i;
+                    SetWindowText(hwndMsgBox, title_text);
+                    Thread.Sleep(1000);
+                }
+
+                // симулируем нажатие "Да"               
+                SendMessageW(hwndMsgBox, WM_COMMAND, (IntPtr)(IDYES | (BN_CLICKED << 16)), hwndButton);
+            }
+
+        }
+
+        public void thread_func()
+        {
+            while (true)
+            {
+                int lastInputTime = GetLastInputTime() / 60;// convert sec to min
+                main.notyfyiconUpdate(lastInputTime);//обновление иконки в трее
+                // form1.PowerModesResume нужен что-бы запустить таймер  выключения при выходе из гибернации
+                if (lastInputTime >= main.minutesOff || main.PowerModesResume)
+                {
+                    Thread thread = new Thread(message_thread_func);
+                    thread.IsBackground = true;
+                    thread.Start();
+
+                    if (MessageBox.Show("Move mouse or press any key \nto interrupt the hibernation ",
+                        "HibernateConfirm", MessageBoxButtons.OKCancel, MessageBoxIcon.Exclamation, MessageBoxDefaultButton.Button2,
+                        MessageBoxOptions.ServiceNotification) != DialogResult.Yes)
+                    {
+                        thread.Abort();
+                        thread.Join();
+                        thread = null;
+                        main.PowerModesResume = false;
+                        continue;
+                    }
+                    thread.Abort();
+                    thread.Join();
+                    thread = null;
+                    Thread.Sleep(1000);
+                    System.Diagnostics.Process.Start("CMD.exe", "/C shutdown -h");
+                    //System.Diagnostics.Process.Start("CMD.exe", "/C "); // test
+
+                }
+                Thread.Sleep(1000);
+
+            }
+        }
+
+
+        [DllImport("User32.dll")]
+        private static extern bool GetLastInputInfo(ref LASTINPUTINFO plii);
+
+        internal struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+        ///возврашяет сколько прошло секунд после последнего нажимания клавиш или движение мышки
+        static int GetLastInputTime()
+        {
+            int idleTime = 0;
+            LASTINPUTINFO lastInputInfo = new LASTINPUTINFO();
+            lastInputInfo.cbSize = (uint)System.Runtime.InteropServices.Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            int envTicks = Environment.TickCount;
+            if (GetLastInputInfo(ref lastInputInfo))
+            {
+                int lastInputTick = (int)lastInputInfo.dwTime;
+                idleTime = envTicks - lastInputTick;
+            }
+            else
+            {
+                //throw new Exception(GetLastError().ToString());
+            }
+            return ((idleTime > 0) ? (idleTime / 1000) : idleTime);// millisec to sec
+        }
+
+    }
+}
